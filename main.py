5c05ad9ba4208e88aa14210a0197ffb1c35661c8
subaishan <su.bai.shan3033@gmail.com>
2026-04-20 19:20:08 +0800

    v1.0.1

--- main.py
+++ main.py
@@ -3,15 +3,24 @@ import sys
 from PySide6.QtWidgets import QApplication
 from main_window import MainWindow
 
-# Important:
-# You need to run the following command to generate the ui_form.py file
+# 重要:
+# 你需要先使用以下命令生成 UI 文件对应的 Python 代码：
 #     pyside6-uic form.ui -o ui_form.py
 #     pyside6-uic settings_dialog_ui.ui -o settings_dialog_ui.py
 #     pyside6-uic export_data.ui -o export_data.py
 #     pyside6-rcc resources.qrc -o resources_rc.py 
-#     pyinstaller --onedir --windowed --name 电负荷预测上位机 --add-data "config.json;." --add-data "icons;icons" main.py
 
-# 测试
+
+#     window下运行以下命令生成带目录的可执行文件 
+#     pyinstaller --onedir --windowed --name 电负荷预测上位机 --add-data "config.json;." --add-data "icons;icons" main.py
+#     window下运行以下命令生成单文件的可执行文件 
+#     pyinstaller --onedir --windowed --name 电负荷预测上位机 --add-data "config.json;." --add-data "icons;icons" main.py
+#     linux下运行以下命令生成带目录的可执行文件
+#     pyinstaller --onedir --windowed --name 电负荷预测上位机 --add-data "config.json:." --add-data "icons:icons" main.py
+#     linux下运行以下命令生成单文件的可执行文件     
+#     pyinstaller --onefile --windowed --name 电负荷预测上位机 --add-data "config.json:." --add-data "icons:icons" main.py
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
